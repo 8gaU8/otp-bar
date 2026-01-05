19f9389bbf0867a55dbf6138ef680f04c5eb19d0
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2026-01-06 01:11:29 +0900

    use config json to determin executable path

--- src/command.ts
+++ src/command.ts
@@ -1,11 +1,15 @@
 import { Command } from "@tauri-apps/plugin-shell";
 
-export async function runOTP(token: string) {
+export async function runOTP(executablePath: string, token: string) {
+  console.log("Using executable path:", executablePath);
+  console.log("Running OTP command with token:", token);
   // refer /src-tauri/capabilities/default.json
-  let result = await Command.create("run-otp", [
-    "--totp",
-    "-b",
-    token,
-  ]).execute();
+  const command = Command.create("bash-exec", [
+    "-c",
+    `${executablePath} --totp -b ${token}`,
+  ]);
+  console.log("Constructed command:", command);
+  const result = await command.execute();
+  console.log("Command execution result:", result);
   return result.stdout;
 }
--- src/otp.ts
+++ src/otp.ts
@@ -1,10 +1,11 @@
 import { writeText } from "@tauri-apps/plugin-clipboard-manager";
 import { runOTP } from "./command.ts";
-import { readToken } from "./config.ts";
+import { getExecutablePath, readToken } from "./config.ts";
 
 export const clipOTP = async (clipID: string) => {
   const token = await readToken(clipID);
-  const otp = await runOTP(token);
+  const executablePath = await getExecutablePath();
+  const otp = await runOTP(executablePath, token);
   console.log("OTP:", otp);
 
   await writeText(otp.trim());
