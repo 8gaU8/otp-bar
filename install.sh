92e3a8b5c8e087356ba1da8f882d5b83df7cb9e6
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2026-01-07 06:04:25 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
-set -euxoC pipefail
+set -eu pipefail
 cd "$(dirname "$0")"
 
 
+# Params
 VERSION="0.2.2"
 TAG="app-v${VERSION}"
 ARCH="aarch64"
 
+
+# Install APP
+echo 'Downloading App DMG...'
 wget "https://github.com/8gaU8/otp-bar/releases/download/${TAG}/OTP.Bar_${VERSION}_${ARCH}.dmg" -O "./OTP-Bar.dmg"
 xattr -d com.apple.quarantine ./OTP-Bar.dmg || true
 hdiutil attach ./OTP-Bar.dmg
@@ -14,11 +18,18 @@ cp -R "/Volumes/OTP Bar/OTP Bar.app" /Applications/
 hdiutil detach "/Volumes/OTP Bar"
 rm ./OTP-Bar.dmg
 
-# config
+# Install Requirements
+echo 'Installing Requirements...'
+brew install coreutils oath-toolkit
+
+
+# Initialize config
+echo 'Initializing config...'
 CONFIG_DIR="${HOME}/.config/otp-bar";
 CONFIG_FILE="config.json";
 
 if [ ! -d "${CONFIG_DIR}" ]; then
+    echo 'Creating config files'
     mkdir -p "${CONFIG_DIR}";
     echo '{ "oathtoolExecutablePath": "/opt/homebrew/bin/oathtool" }'> "${CONFIG_DIR}/${CONFIG_FILE}";
 fi
